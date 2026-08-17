eaf2277cb05e74ca88fcb6d324b7e607a2b0addd
szandor <szandor25@gmail.com>
2026-08-17 13:32:08 +0200

    Initial Commit

---
+++ BZZT!/BZZT_App.swift
@@ -0,0 +1,17 @@
+//
+//  BZZT_App.swift
+//  BZZT!
+//
+//  Created by Arkadiusz Woiński on 17/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct BZZT_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BZZT!Tests/BZZT_Tests.swift
@@ -0,0 +1,19 @@
+//
+//  BZZT_Tests.swift
+//  BZZT!Tests
+//
+//  Created by Arkadiusz Woiński on 17/08/2026.
+//
+
+import Testing
+@testable import BZZT_
+
+struct BZZT_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
